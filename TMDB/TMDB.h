cc5b63b051ac6916e3b9bdef4e4b2acdd7537f56
David Luna <69691740+piroworkz@users.noreply.github.com>
2025-02-07 17:47:35 -0600

    Initial Commit

---
+++ TMDB/TMDB.h
@@ -0,0 +1,18 @@
+//
+//  TMDB.h
+//  TMDB
+//
+//  Created by David Luna on 07/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TMDB.
+FOUNDATION_EXPORT double TMDBVersionNumber;
+
+//! Project version string for TMDB.
+FOUNDATION_EXPORT const unsigned char TMDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDB/PublicHeader.h>
+
+
