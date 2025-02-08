69eda22d3c9bd6057150002b34ec64f12a1ce300
David Luna <69691740+piroworkz@users.noreply.github.com>
2025-02-08 07:53:26 -0600

    Remove URL validation on test URL protocol stub should intercept all request

--- TMDBTests/data/framework/URLSessionHttpClientTests.swift
+++ TMDBTests/data/framework/URLSessionHttpClientTests.swift
@@ -31,7 +31,7 @@ final class URLSessionHttpClientTests: XCTestCase {
         URLProtocol.registerClass(URLProtocolStub.self)
         let url = URL(string: "https://example.com")!
         let error = NSError(domain: "", code: 0)
-        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+        URLProtocolStub.stub(data: nil, response: nil, error: error)
         let sut = URLSessionHttpClient()
         
         let expectation = expectation(description: "Wait for request to complete")
@@ -52,7 +52,7 @@ final class URLSessionHttpClientTests: XCTestCase {
     
     
     class URLProtocolStub: URLProtocol {
-        private static var stubs = [URL: Stub]()
+        private static var stub: Stub?
         
         private struct Stub {
             let data: Data?
@@ -60,14 +60,21 @@ final class URLSessionHttpClientTests: XCTestCase {
             let error: Error?
         }
         
-        static func stub(url: URL,data: Data?, response: URLResponse?, error: Error?) {
-            stubs[url] = Stub(data: data, response: response, error: error)
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
+        static func startInterceptingRequests() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptingRequests() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            stub = nil
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
-            guard let url = request.url else { return false }
-            
-            return URLProtocolStub.stubs[url] != nil
+            true
         }
         
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -75,17 +82,16 @@ final class URLSessionHttpClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
             
-            if let data = stub.data {
+            if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
             
-            if let response = stub.response {
+            if let response =  URLProtocolStub.stub?.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
             
-            if let error = stub.error {
+            if let error =  URLProtocolStub.stub?.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
             
