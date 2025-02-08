aec5e4257e8e9c74b8c1c16d1095bdf4f19e5ebb
David Luna <69691740+piroworkz@users.noreply.github.com>
2025-02-07 22:37:09 -0600

    Implemented URLProtocol stubing instead of subclassing URLSession

--- TMDBTests/data/framework/URLSessionHttpClientTests.swift
+++ TMDBTests/data/framework/URLSessionHttpClientTests.swift
@@ -6,43 +6,92 @@
 //
 
 import XCTest
+import TMDB
 
 class URLSessionHttpClient {
     private let session: URLSession
-
-    init(session: URLSession) {
+    
+    init(session: URLSession = .shared) {
         self.session = session
     }
-
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }.resume()
+    
+    func get(from url: URL, completion: @escaping (HttpClientResult) -> Void) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 final class URLSessionHttpClientTests: XCTestCase {
     
-    func test_GIVEN_sut_WHEN_getIsCalled_THEN_shouldCreateDataTask() {
-        let url = URL(string: "https://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHttpClient(session: session)
-
-        sut.get(from: url)
-
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-
-    class URLSessionSpy: URLSession {
-        var receivedURLs: [URL] = []
-        private let mockTask = FakeURLSessionDataTask()
+    
+    func test_GIVEN_sut_WHEN_getIsCalled_THEN_shouldFailRequestWithError() {
+        URLProtocol.registerClass(URLProtocolStub.self)
+        let url = URL(string: "https://example.com")!
+        let error = NSError(domain: "", code: 0)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+        let sut = URLSessionHttpClient()
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
-            return mockTask
+        let expectation = expectation(description: "Wait for request to complete")
+        
+        sut.get(from: url) { result in
+            switch result {
+            case .failure(let receivedError as NSError):
+                XCTAssertEqual(receivedError.code, error.code)
+            default:
+                XCTFail("Expected failure with error \(error) but got \(result) instead")
+            }
+            expectation.fulfill()
         }
         
+        wait(for: [expectation], timeout: 1.0)
+        URLProtocol.unregisterClass(URLProtocolStub.self)
     }
-
-    class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {}
+    
+    
+    class URLProtocolStub: URLProtocol {
+        private static var stubs = [URL: Stub]()
+        
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        static func stub(url: URL,data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            guard let url = request.url else { return false }
+            
+            return URLProtocolStub.stubs[url] != nil
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        override func startLoading() {
+            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = stub.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() { }
     }
 }
