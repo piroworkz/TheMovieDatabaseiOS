d9008feb20d082ebd30f71d542a3f0a5824bdf94
David Luna <69691740+piroworkz@users.noreply.github.com>
2025-02-07 18:15:47 -0600

    test_GIVEN_sutIsInitialized_WHEN_loadIsCalled_THEN_shouldMakeRequestToProvidedUrl

--- TMDBTests/data/remote/RemoteCatalogLoaderTests.swift
+++ TMDBTests/data/remote/RemoteCatalogLoaderTests.swift
@@ -8,20 +8,46 @@
 import XCTest
 @testable import TMDB
 
-class RemoteCatalogLoader {}
+class RemoteCatalogLoader {
+    private let client: HttpClient
+    
+    init(client: HttpClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://api.themoviedb.org/3/movie/popular")!)
+    }
+}
 
-class HttpClient {
+protocol HttpClient {
+    func get(from url: URL)
+}
+
+class HttpClientSpy: HttpClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteCatalogLoaderTests: XCTestCase {
     
-    
     func test_GIVEN_sut_WHEN_initialized_THEN_shouldNotRequestDataFromAPI() {
-        let client = HttpClient()
-        let _ = RemoteCatalogLoader()
+        let client = HttpClientSpy()
+        let _ = RemoteCatalogLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_GIVEN_sutIsInitialized_WHEN_loadIsCalled_THEN_shouldMakeRequestToProvidedUrl() {
+        let client = HttpClientSpy()
+        let sut = RemoteCatalogLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
