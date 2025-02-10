27f5727ecf3dfbf256c115ef15220da7f7e3dbb0
David <69691740+piroworkz@users.noreply.github.com>
2025-02-10 15:28:23 -0600

    shouldRequestCacheDeletion
    test_GIVEN_sut_WHEN_saveIsCalled_THEN_shouldRequestCacheDeletion

--- TMDBTests/framework/local/CacheCatalogUseCaseTest.swift
+++ TMDBTests/framework/local/CacheCatalogUseCaseTest.swift
@@ -6,11 +6,18 @@
 //
 
 import XCTest
+import TMDB
 
 class LocalCatalogLoader {
     
+    private let store: CatalogStore
+    
     init(store: CatalogStore) {
         self.store = store
+    }
+    
+    func save(_ catalog: Catalog) {
+        store.deleteCachedCatalog()
     }
 }
 
@@ -18,6 +25,9 @@ class LocalCatalogLoader {
 class CatalogStore {
     var deleteCachedCatalogCount = 0
     
+    func deleteCachedCatalog() {
+        deleteCachedCatalogCount += 1
+    }
 }
 
 final class CacheCatalogUseCaseTest: XCTestCase {
@@ -27,5 +37,29 @@ final class CacheCatalogUseCaseTest: XCTestCase {
         _ = LocalCatalogLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedCatalogCount, 0)
+    }
+    
+    
+    func test_GIVEN_sut_WHEN_saveIsCalled_THEN_shouldRequestCacheDeletion() {
+        let store = CatalogStore()
+        let sut = LocalCatalogLoader(store: store)
+        let catalog = createCatalog()
+        
+        sut.save(catalog)
+        
+        XCTAssertEqual(store.deleteCachedCatalogCount, 1)
+    }
+    
+}
+
+extension CacheCatalogUseCaseTest {
+    
+    func createCatalog(_ count: Int = 4) -> Catalog {
+        return Catalog(page: 1, totalPages: 0, catalog: (0...count).map { self.createMovie(id: $0) })
+    }
+    
+    
+    func createMovie(id: Int) -> Movie {
+        return Movie(id: id, title: "Title \(id)", posterPath: "fake poster path \(id)")
     }
 }
