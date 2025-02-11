28eccb71f5b60b355455cf1e45935ed66b383adf
David <69691740+piroworkz@users.noreply.github.com>
2025-02-11 15:09:34 -0600

    LoadCatalogFromCacheTests

---
+++ TMDBTests/framework/local/datasource/load/LoadCatalogFromCacheTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoadCatalogFromCacheTests.swift
+//  TMDBTests
+//
+//  Created by David Luna on 11/02/25.
+//
+
+import XCTest
+
+final class LoadCatalogFromCacheTests: XCTestCase {
+    
+    
+}
