62a684550ab15d30e650c624bba100ce6639f74e
David <69691740+piroworkz@users.noreply.github.com>
2025-02-15 22:49:30 -0600

    Updated CatalogStoreSpecs

--- TMDB/MoviesFeature/framework/local/core_data/CoreDataCatalogExtensions.swift
+++ TMDB/MoviesFeature/framework/local/core_data/CoreDataCatalogExtensions.swift
@@ -7,7 +7,7 @@
 import CoreData
 
 internal extension NSPersistentContainer {
-    internal enum LoadingError: Swift.Error {
+    enum LoadingError: Swift.Error {
         case modelNotFound
         case failedToLoadPersistentStores(Swift.Error)
     }
