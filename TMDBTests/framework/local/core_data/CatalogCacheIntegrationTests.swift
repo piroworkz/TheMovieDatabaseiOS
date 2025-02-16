cf340e0887349e9469cc1fae62359e1d7501c023
David <69691740+piroworkz@users.noreply.github.com>
2025-02-16 12:09:25 -0600

    Create CatalogCacheIntegrationTests.swift

---
+++ TMDBTests/framework/local/core_data/CatalogCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  CatalogCacheIntegrationTests.swift
+//  TMDBTests
+//
+//  Created by David Luna on 16/02/25.
+//
+
+import XCTest
+
+final class CatalogCacheIntegrationTests: XCTestCase {
+    
+}
